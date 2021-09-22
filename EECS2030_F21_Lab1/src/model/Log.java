8878b93f2ff332bf2e6e990f06f9ba81f4ea6d00
Samir Gandhi <77183760+gandhi-samir@users.noreply.github.com>
2021-09-22 13:06:36 -0400

    lab 1

---
+++ EECS2030_F21_Lab1/src/model/Log.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Log {
+	String verion="";
+
+	public Log(String version) {
+		this.verion=version;
+	}
+
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getNumberOfFixes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getFixes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
